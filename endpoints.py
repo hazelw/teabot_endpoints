0c2eed3a9043774ee8bc99b7d245980b7d6b29f3
Aaron Kalair <aaronkalair@gmail.com>
2016-04-16 11:40:59 +0100

    feat: noticket - Switch to using Slack

--- endpoints.py
+++ endpoints.py
@@ -1,24 +1,22 @@
 from flask import Flask, Response, jsonify
-from HipchatCommunicator import HipchatCommunicator
+from SlackCommunicator import SlackCommunicator
 from models import BrewLog
 from settings import AUTH_TOKEN, ROOM_ID, USER_NAME, DEBUG
 
 app = Flask(__name__)
-hipchat_communicator = HipchatCommunicator(
-    auth_token=AUTH_TOKEN, room_id=ROOM_ID, user_name=USER_NAME
-)
+slack_communicator = SlackCommunicator()
 
 
 @app.route("/newpot")
 def newPot():
-    hipchat_communicator.post_message_to_room("Teapot is ready")
+    slack_communicator.post_message_to_room("Teapot is ready")
     BrewLog.create()
     return Response()
 
 
 @app.route("/lastcall")
 def lastCall():
-    hipchat_communicator.post_message_to_room(
+    slack_communicator.post_message_to_room(
         "Teapot is about to go cold drink up"
     )
     return Response()
